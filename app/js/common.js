14c0c2d4c34570d5b54ab9fe73d0165d7b8cfe01
Developer <dhcp_book@mail.ru>
2018-03-04 20:05:04 +0300

    grid+header-nav+aside-nav+footer

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
+    // $('.nav-item').on('mouseover', 'img', function() {
+	 //    var attrStr = $(this).attr('src');
+	 //   $(this).attr('src', attrStr.replace(".", "-red."));
+    // });
+    // $('.nav-item').on('mouseout', 'img', function() {
+    //     var attrStr = $(this).attr('src');
+    //     $(this).attr('src', attrStr.replace("-red.", "."));
+    // });
 
+    // $('#chicken').attr('src', 'img/002-chicken-red.png');
+    // $('#sausage').attr('src', 'img/001-packed-sausage-red.png');
 });
