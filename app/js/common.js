e4da8de82e56671dd5a2bb992d7850441c1cc9fa
Developer <dhcp_book@mail.ru>
2018-03-05 10:22:18 +0300

    just commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,11 @@
-$(function() {
 
-    // $('.nav-item').on('mouseover', 'img', function() {
-	 //    var attrStr = $(this).attr('src');
-	 //   $(this).attr('src', attrStr.replace(".", "-red."));
-    // });
-    // $('.nav-item').on('mouseout', 'img', function() {
-    //     var attrStr = $(this).attr('src');
-    //     $(this).attr('src', attrStr.replace("-red.", "."));
-    // });
+    $(document).ready(function(){
 
-    // $('#chicken').attr('src', 'img/002-chicken-red.png');
-    // $('#sausage').attr('src', 'img/001-packed-sausage-red.png');
-});
+        $('.products-carousel').owlCarousel({
+            nav: true,
+            smartSpeed: 700,
+            items: 1
+        });
+
+    });
+
